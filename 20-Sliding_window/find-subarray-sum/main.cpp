b513322ae3c9d2cffc4f44f64c5072ab27814fb3
Takudzwa Masendeke <80186418+takumasendeke@users.noreply.github.com>
2026-07-15 10:21:27 +0200

    feat: implemented a sliding window algorithm to solve a subarry sum problem

---
+++ 20-Sliding_window/find-subarray-sum/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::vector;
+
+vector<int> subarraySum(const vector<int>& nums,int target){
+    vector<int> solution = {0,0};
+    int length = nums.size();
+    int currentSum = 0;
+    int start = 0;
+
+    for (int end = 0; end < length ; end++){
+        currentSum += nums[end];
+
+        if (currentSum == target){
+            solution[0] = start;
+            solution[1] = end;
+            return solution;
+        }
+
+        while(currentSum > target){
+            currentSum -= nums[start];
+            start++;
+        }
+    }
+    return solution;
+}
+
+int main(){
+
+    vector<int> nums = {3,1,4,9,2,1,7,5};
+    int target = 10;
+
+    cout << "[";
+    for(int num: subarraySum(nums,target)){
+        cout << num << " ";
+    }
+     cout << "]";
+
+    return 0;
+}
